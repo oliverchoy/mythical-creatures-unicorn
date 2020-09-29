9ec3b9b9b6209b620002cf354afac46948f91023
Oliver Choy <oliver.choy@slalom.com>
2020-09-29 01:22:45 -0700

    Changes to lambda to reflect name

--- hello_lambda.py
+++ hello_lambda.py
@@ -3,7 +3,7 @@ import requests
 def lambda_handler(json_input, context):
     print(json_input)
     print(context)
-    print("Hello Unicorn")
+    print("Hello Unicorn A")
 
 
 if __name__ == '__main__':
